d310cc3399ec8c55a60285e4601e3b1c1e7d77d7
Jui-Wen, Yeh <B05208038@ntu.edu.tw>
2019-12-27 15:24:41 +0800

    add new things and plotting

--- try_analysis.m
+++ try_analysis.m
@@ -4,17 +4,23 @@
 clc; clear all
 pic = imread ('SharedScreenshot.jpg'); 
 figure (1)
-imshow(pic)
+% imshow(pic)
 gray_pic = rgb2gray(pic); 
 [size_pic_x, size_pic_y] = size (gray_pic); 
 R_shell_pic = uint8(pic(:, :, 1));
 G_shell_pic = uint8(pic(:, :, 2));
 B_shell_pic = uint8(pic(:, :, 3)); 
 
+figure (2)
+cut_x = 10; 
+cut_y = 95; 
+cropped = imcrop(pic, [cut_y cut_x size_pic_y-cut_y size_pic_x-cut_x]);
+imshow(cropped)
+
 % test out substract method
 R = imsubtract(R_shell_pic, gray_pic); 
 figure (200)
-R = im2bw(R, 0.32);
+R = im2bw(R, 0.30);
 imshow(R)
 
 %do the video processing 
@@ -25,9 +31,10 @@ get (video_1)
 mkdir Frame_by_frame
 
 %cut video into pcitures and substract red dot
-NumberOfFrames =uint8( video_1.Duration .* video_1.FrameRate); 
+NumberOfFrames =uint8(video_1.Duration .* video_1.FrameRate)-2; 
 video_tear_down = zeros(video_1.Height, video_1.Width, NumberOfFrames);
 video_tear_down_process = video_tear_down; 
+plot_point = zeros(NumberOfFrames, 2);
 
 for img = 1:NumberOfFrames
     process  = read(video_1, img); 
@@ -35,16 +42,67 @@ for img = 1:NumberOfFrames
     gray_process = rgb2gray(process);
     R_shell_process = uint8(process(:, :, 1));    
     R_process = imsubtract(R_shell_process, gray_process); 
-    R_process = im2bw(R_process, 0.32);
+    R_process = im2bw(R_process, 0.34);
     video_tear_down_process(:, :, img) = R_process;
+    cut_x = 30; 
+    cut_y =130; 
+    cropped = imcrop(R_process, [cut_y cut_x size_pic_y-cut_y size_pic_x-cut_x]);
+    %find the centroid of the figure
+    R_process = imfill(R_process,'holes');
+    se = strel('line',5,5);
+    R_process = imerode(R_process,se);
+    L = bwlabel(R_process,8);
+   s = regionprops(R_process,'centroid');
+   theta10=[];
+   theta20=[];
+   hold on;
     
+   centroids = cat(1,s.Centroid);  
+   Csize=size(centroids);
+   dotnumber=Csize(1);
+   figure(4);
+     
+    if (dotnumber==3)
+        x1=centroids(1,1);x2=centroids(2,1);x3=centroids(3,1);
+        y1=centroids(1,2);y2=centroids(2,2);y3=centroids(3,2);
+        theta1=atan((x2-x1)/(y2-y1));
+        theta2=atan((x3-x2)/(y3-y2))+theta1; 
+        plot(img,theta1,'r^');
+        plot(img,theta2,'b^');
+        
+    elseif (dotnumber==4)
+        x1=centroids(1,1);
+        x2=(centroids(2,1)+centroids(3,1))/2;
+        x3=centroids(4,1);
+        y1=centroids(1,2);
+        y2=(centroids(2,2)+centroids(3,2))/2;;
+        y3=centroids(4,2);
+        theta1=atan((x2-x1)/(y2-y1));
+        theta2=atan((x3-x2)/(y3-y2))+theta1; 
+        %plot(img,theta1,'ro');
+        %plot(img,theta2,'bo');
+    end
+    plot_point(img, 1) = theta1; 
+    plot_point(img, 2) = theta2;
+    
+    %plot_point 
+    i=i+1;
     %save file 
+    hold off
     cd Frame_by_frame
     filename=strcat('frame',num2str(img),'.jpg');
     imwrite(R_process,filename);
     cd ..
-    
-    figure(500)
-    imshow(R_process)
-
 end 
+
+figure(1000)
+t = linspace(0, video_1.Duration, NumberOfFrames);
+
+theta_1 = plot_point(:,1).';
+theta_2 = plot_point(:,2).'; 
+plot(t,theta_1, 'r-o', t, theta_2,  'b-*');
+title('experimental response')
+xlabel('time (sec)')
+ylabel('theta(rad)')
+legend('theta 1','theta 2')
+
